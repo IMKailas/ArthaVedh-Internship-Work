e8a414c449c82dea0c7171c4864796ca5a22cf0f
Swarajhere <swaraj.nalawade2004@gmail.com>
2025-01-17 12:27:58 +0530

    Update config.py

--- Swaraj_Nalawade/Straddles_and_Strangles/config.py
+++ Swaraj_Nalawade/Straddles_and_Strangles/config.py
@@ -1,4 +1,9 @@
+
 # config.py
+
+# File Settings
+DATA_FILE = r"C:\assignement\sem5\Internship\Straddles_and_Strangles\loadData.csv"  # Your input CSV file name
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"  # Date format in CSV
 
 # Trading Account Settings
 INITIAL_BALANCE = 10000
@@ -6,49 +11,25 @@ STOP_LOSS_PCT = 0.5
 TARGET_PROFIT_PCT = 0.5
 BALANCE_RISK_THRESHOLD = 0.7  # Stop trading if balance drops below 70% of initial
 
-# Market Data Simulation Parameters
-PRICE_RANGE = {
-    "MIN_BID": 100,
-    "MAX_BID": 102,
-    "MIN_ASK": 101,
-    "MAX_ASK": 102
-}
-
-VOLUME_RANGE = {
-    "MIN": 500,
-    "MAX": 2000
-}
-
-ORDER_FLOW_RANGE = {
-    "MIN": 50,
-    "MAX": 200
-}
-
-# Strategy-Specific Parameters
+# Strategy Parameters
 STRADDLE_PARAMS = {
     "MAX_SPREAD": 0.5,
     "MIN_VOLUME": 800,
-    "MIN_IMPLIED_VOLATILITY": 0.3,
-    "IMPLIED_VOLATILITY_RANGE": {
-        "MIN": 0.2,
-        "MAX": 0.5
-    },
-    "DELTA_RANGE": {
-        "MIN": -0.5,
-        "MAX": 0.5
-    }
+    "MIN_IMPLIED_VOLATILITY": 30,  # RSI can be used as a volatility indicator
+    "RSI_OVERSOLD": 30,
+    "RSI_OVERBOUGHT": 70
 }
 
 STRANGLE_PARAMS = {
     "MAX_SPREAD": 0.5,
     "MIN_VOLUME": 500,
-    "MIN_ORDER_FLOW_RATIO": 1.1
+    "MIN_ORDER_FLOW_RATIO": 1.1,
+    "VOLUME_MA_MULTIPLIER": 1.2  # Volume should be 20% above MA for signal
 }
 
-# Simulation Parameters
-PRICE_SIMULATION = {
-    "MIN_CHANGE": -0.5,
-    "MAX_CHANGE": 1.0
-}
+# Technical Indicators
+MOVING_AVERAGE_PERIOD = 20  # For Volume MA
+RSI_PERIOD = 14
 
-UPDATE_INTERVAL = 1  # Time between market data updates (seconds)
+# Update Interval (in seconds)
+UPDATE_INTERVAL = 1
